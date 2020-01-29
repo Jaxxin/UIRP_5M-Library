27b12949acecc8641c5e98a39ae7b0aac757db99
Jaxxin <47609016+Jaxxin@users.noreply.github.com>
2020-01-29 20:09:34 +0200

    ESX Inventoryhud Trunk Update
    This script is updated to the new ESX weight system.
    
    There are some issues with refreshing the items.
    You need to re-open the trunk when you place something inside or take it out.
    This issue is not an actual bug and its not possible to duplicate items with it.

---
+++ esx_inventoryhud_trunk/locales/en.lua
@@ -0,0 +1,13 @@
+Locales["en"] = {
+  ["trunk_closed"] = "Този багажник е затворен.",
+  ["no_veh_nearby"] = "Няма автомобил наблизо.",
+  ["trunk_in_use"] = "Този багажник вече се ползва.",
+  ["trunk_full"] = "Багажникът е пълен.",
+  ["invalid_quantity"] = "Невалидна стойност",
+  ["cant_carry_more"] = "Нямаш място.",
+  ["nacho_veh"] = "Този автомобил не е твой.",
+  ["invalid_amount"] = "Невалидна стойност",
+  ["insufficient_space"] = "Insufficient space",
+  ["trunk_info"] = "<h3>Vehicle trunk</h3><br><strong>Plate:</strong> %s<br><strong>Capacity:</strong> %s / %s",
+  ["player_inv_no_space"] = "Няма достатъчно място за този предмет в инвентара!"
+}
